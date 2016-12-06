b256d1138f32d6c3a2b58c161c06baab42f72296
Tomáš Mikula <mail@tomasmikula.cz>
2016-12-06 14:39:20 +0100

    Implement basic connection to Redis db
    Resolve #4

---
+++ crawler/db.py
@@ -0,0 +1,45 @@
+import redis
+from . import url
+
+db = redis.Redis(host='localhost', port=6379, db=1)
+db_visited = redis.Redis(host='localhost', port=6379, db=2)
+
+def insert_url(url):
+    """Insert url into db and set visited False and inlinks 0"""
+    args = {'url': url.encode('utf-8')}
+
+    return db.hmset(url, args)
+
+# def get_url(url):
+#     """Return url from db"""
+#     result = db.hmget(url, 'url')
+#
+#     if result[0] is None:
+#         return None
+#
+#     return result[0].decode('utf-8')
+
+def delete_url(url):
+    """Try to delete url from db, returns True if case of success"""
+    result = db.delete(url)
+
+    return True if result == 1 else False
+
+def exists_url(url):
+    """Return if url is exists in db"""
+    if (db.exists(url) > 0) or (db_visited.exists(url) > 0):
+        return True
+    else:
+        return False
+
+def set_visited_url(url):
+    """Try to set url to visited"""
+    if delete_url(url) is True:
+        db_visited.hmset(url, {'url': url})
+        return True
+    else:
+        return False
+
+def flush_db():
+    """Delete everything from database"""
+    return db.flushall(), db_visited.flushall()
--- unittests.py
+++ unittests.py
@@ -1,11 +1,46 @@
 import unittest
+import redis
+from unittest.mock import patch
+
 from crawler import url
+from crawler import db
 
 class TestUrlMethods(unittest.TestCase):
 
     def test_url_clean(self):
         self.assertEqual(url.clean("http://upol.cz/"), "http://upol.cz")
         self.assertEqual(url.clean("http://upol.cz"), "http://upol.cz")
+
+@patch('crawler.db.db', redis.Redis(host='localhost', port=6379, db=10))
+@patch('crawler.db.db_visited', redis.Redis(host='localhost', port=6379, db=11))
+class TestDbMethods(unittest.TestCase):
+    def setUp(self):
+        self.url = "http://upol.cz"
+
+    def test_url_inser(self):
+        self.assertFalse(db.exists_url(self.url))
+        self.assertTrue(db.insert_url(self.url))
+        self.assertTrue(db.exists_url(self.url))
+
+    def test_url_delete(self):
+        db.insert_url("http://test.com")
+        self.assertEqual(db.delete_url("http://test.com"), True)
+        self.assertEqual(db.delete_url("Not exists"), False)
+
+    def test_url_exists(self):
+        db.insert_url(self.url)
+        self.assertTrue(db.exists_url(self.url))
+        self.assertFalse(db.exists_url("Not exists"))
+
+    def test_url_set_visited(self):
+        self.assertFalse(db.exists_url(self.url))
+        self.assertFalse(db.set_visited_url(self.url))
+        db.insert_url(self.url)
+        self.assertTrue(db.set_visited_url(self.url))
+        self.assertTrue(db.exists_url(self.url))
+
+    def tearDown(self):
+        db.flush_db()
 
 if __name__ == '__main__':
     unittest.main()
