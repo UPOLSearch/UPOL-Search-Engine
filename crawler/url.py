1dc449ddd1587596da5c2286b32d0c9228129a2b
Tomáš Mikula <mail@tomasmikula.cz>
2016-12-06 11:57:29 +0100

    Implement clean url
    Resolve #6

--- crawler/url.py
+++ crawler/url.py
@@ -3,3 +3,7 @@ import hashlib
 def hash(url):
     """Returns hash of url"""
     return hashlib.sha1(url.encode('utf-8')).hexdigest()
+
+def clean(url):
+    """Remove last backslash from url"""
+    return url.rstrip('/')
