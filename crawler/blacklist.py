84cf4aac69bfae5706645be5950c0050d4bec553
Tomáš Mikula <mail@tomasmikula.cz>
2016-12-07 19:12:54 +0100

    Blacklist cosmetics

--- crawler/blacklist.py
+++ crawler/blacklist.py
@@ -1,8 +1,11 @@
 from crawler import urls
 
 #TODO - load values from file
-blacklist = ["portal.upol.cz", "stag.upol.cz", "stagservices.upol.cz", "courseware.upol.cz", "helpdesk.upol.cz"]
+blacklist = ["portal.upol.cz",
+            "stag.upol.cz",
+            "stagservices.upol.cz",
+            "courseware.upol.cz",
+            "helpdesk.upol.cz"]
 
 def is_url_blocked(url):
     return True if urls.domain(url) in blacklist else False
-    
