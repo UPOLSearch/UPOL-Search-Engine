f0a705ca589061bc3a4fa5e7ae8bba34a73a3168
Tomáš Mikula <mail@tomasmikula.cz>
2016-12-11 13:15:05 +0100

    Implement config file

--- crawler/config.py
+++ crawler/config.py
@@ -1,4 +1,12 @@
 from crawler.urls import url_tools
 
-limit_domain = "http://upol.cz"
+# Info
+version = "0.3-dev"
+project_url = "https://github.com/UPOLSearch/UPOL-Crawler"
+user_agent = "Mozilla/5.0 (compatible; UPOL-Crawler/" + version + "; +" + project_url + ")"
+
+# Settings
+limit_domain = "http://inf.upol.cz"
 regex = url_tools.generate_regex(limit_domain)
+# for requests
+verify_ssl = False
