9989efc1f4df1e3558df23edd8a68b55d13bccaf
Tomáš Mikula <mail@tomasmikula.cz>
2016-12-11 19:53:02 +0100

    Add debug log for not valid url

--- crawler/urls/parser.py
+++ crawler/urls/parser.py
@@ -98,5 +98,9 @@ def validated_page_urls(soup, url):
 
         if validator.validate(link_url):
             valid_urls.add(link_url)
+        else:
+            with open("not_valid.txt", "a") as log_file:
+                log_file.write("URL: " + url)
+                log_file.write("\n")
 
     return valid_urls
