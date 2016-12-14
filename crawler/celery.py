e039507fc8556eb5f800692949137c504c41be51
Tomáš Mikula <mail@tomasmikula.cz>
2016-12-14 17:50:15 +0100

    Adjust queue

--- crawler/celery.py
+++ crawler/celery.py
@@ -6,14 +6,14 @@ from kombu import Queue, Exchange
 class Config(object):
     broker_url = "amqp://guest:guest@localhost:5672//"
 
-    # task_queues = (
-    #     Queue(
-    #         'important',
-    #         exchange=Exchange('important'),
-    #         routing_key="important",
-    #         queue_arguments={'x-max-length': 10}
-    #     ),
-    # )
+    task_queues = (
+        Queue(
+            'crawler',
+            exchange=Exchange('crawler'),
+            routing_key="crawler",
+            queue_arguments={'x-max-length': 2000}
+        ),
+    )
 
     enable_utc = True
     timezone = 'Europe/Prague'
