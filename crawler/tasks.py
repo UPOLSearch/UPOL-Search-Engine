760c9c97ab870fa45712599ef6130fdcee7a282f
Tomáš Mikula <mail@tomasmikula.cz>
2016-12-18 10:23:44 +0100

    Turn off loggs in celery

--- crawler/tasks.py
+++ crawler/tasks.py
@@ -1,16 +1,17 @@
 from __future__ import absolute_import, unicode_literals
 from .celery import app
-from celery.utils.log import get_task_logger
+# from celery.utils.log import get_task_logger
 from .crawler import crawl_url
 
-logger = get_task_logger(__name__)
+# logger = get_task_logger(__name__)
 
 
 @app.task(rate_limit="6/s", queue='crawler')
 def crawl_url_task(url):
-    response, status, redirected = crawl_url(url)
-    if response is not None:
-        logger.info(str(url) + " | " + str(response.status_code) + " | " + str(response.reason) +
-                    " | " + str(response.headers['Content-Type']) + " | " + str(status) + " | Redirected: " + str(redirected))
-    else:
-        logger.info(url + " | " + str(status) + " | Redirected: " + str(redirected))
+    crawl_url(url)
+    # response, status, redirected = crawl_url(url)
+    # if response is not None:
+    #     logger.info(str(url) + " | " + str(response.status_code) + " | " + str(response.reason) +
+    #                 " | " + str(response.headers['Content-Type']) + " | " + str(status) + " | Redirected: " + str(redirected))
+    # else:
+    #     logger.info(url + " | " + str(status) + " | Redirected: " + str(redirected))
