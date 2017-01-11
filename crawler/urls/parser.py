e180610917ed3ab0997269692db7153077ac5e64
Tomáš Mikula <mail@tomasmikula.cz>
2017-01-11 09:19:14 +0100

    First construct absolute url

--- crawler/urls/parser.py
+++ crawler/urls/parser.py
@@ -132,17 +132,15 @@ def validated_page_urls(soup, url):
 
         link_url = link['href']
 
+        if not url_tools.is_url_absolute(link_url):
+            link_url = urllib.parse.urljoin(page_base_url, link_url)
+
         scheme, netloc, path, qs, anchor = urllib.parse.urlsplit(link_url)
 
         if not scheme:
             link_url = url_tools.add_scheme(url)
 
-        if not url_tools.is_url_absolute(link_url):
-            if "www" in link_url:
-                e = ValueError(url + " absolute url is invalid " + link_url)
-                crawler.tasks.log_url_validator_task.delay(url, "exception", str(e))
-                # raise e
-            link_url = urllib.parse.urljoin(page_base_url, link_url)
+
 
         link_url = url_tools.clean(link_url)
 
