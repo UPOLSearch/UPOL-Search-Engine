13390a6c0d3fb41c9f3369d003558e034fbf02d1
Tomáš Mikula <mail@tomasmikula.cz>
2017-02-10 17:51:36 +0100

    Add more tests to test_parser

--- tests/test_parser.py
+++ tests/test_parser.py
@@ -13,3 +13,17 @@ from upol_crawler.urls import parser
 def test_is_page_wiki(html, result):
     soup = BeautifulSoup(html, "lxml")
     assert parser.is_page_wiki(soup) == result
+
+
+@pytest.mark.parametrize('html, result', [
+    ('<a href="aaaa" rel="nofollow">', False),
+    ('<a href="aaaa" rel="bookmark">', False),
+    ('<a href="aaaa" rel="alternate">', False),
+    ('<a href="aaaa" rel="license">', False),
+    ('<a href="aaaa" rel="search">', False),
+    ('<a href="aaaa">', True),
+    ])
+def test_check_rel_attribute(html, result):
+    soup = BeautifulSoup(html, "lxml")
+    links = soup.find_all('a', href=True)
+    assert parser.check_rel_attribute(links[0]) == result
