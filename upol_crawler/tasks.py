e18ae218ac6e85b044c85ada97e1f54b58dfa864
Tomáš Mikula <mail@tomasmikula.cz>
2017-02-11 15:33:40 +0100

    Fix circular imports

--- upol_crawler/tasks.py
+++ upol_crawler/tasks.py
@@ -8,16 +8,15 @@ import time
 
 
 from celery.utils.log import get_task_logger
-import upol_crawler.core.crawler
-import upol_crawler.tools.logger
 from upol_crawler.celery import app
-# from upol_crawler.tools import logger
-# from upol_crawler.core import crawler
+
 from upol_crawler.settings import *
 
 
 @app.task(rate_limit=CONFIG.get('Settings', 'crawl_frequency'), queue='crawler', ignore_result=True, task_compression='zlib')
 def crawl_url_task(url, depth):
+    from upol_crawler.core import crawler
+
     if CONFIG.getboolean('Debug', 'cprofile_crawl_task'):
         os.makedirs(CPROFILE_DIR, exist_ok=True)
 
@@ -26,14 +25,15 @@ def crawl_url_task(url, depth):
         cprofile_filename = hashlib.sha1(actual_time).hexdigest()
         cprofile_path = os.path.join(CPROFILE_DIR, cprofile_filename)
 
-        cProfile.runctx('upol_crawler.core.crawler.crawl_url(url, depth)',
+        cProfile.runctx('crawler.crawl_url(url, depth)',
                         globals=globals(),
                         locals=locals(),
                         filename=cprofile_path)
     else:
-        upol_crawler.core.crawler.crawl_url(url, depth)
+        crawler.crawl_url(url, depth)
 
 
 @app.task(queue='logger', ignore_result=True, task_compression='zlib')
 def log_url_reason_task(url, reason, arg={}):
-    upol_crawler.tools.logger.log_url_reason(url, reason, arg)
+    from upol_crawler.tools import logger
+    logger.log_url_reason(url, reason, arg)
