637c2dada71296d9e135cefeae86eb17e52cb2e8
Tomáš Mikula <mail@tomasmikula.cz>
2017-02-15 12:04:36 +0100

    Create logs folder if not exists

--- upol_crawler/tools/logger.py
+++ upol_crawler/tools/logger.py
@@ -8,7 +8,10 @@ def universal_logger(name):
     logger = logging.getLogger(name)
     logger.setLevel(logging.DEBUG)
 
-    handler = logging.FileHandler(os.path.join(os.path.join(ROOT_DIR, 'logs/'), name + '.log'), 'a')
+    logs_path = os.path.join(ROOT_DIR, 'logs/')
+    os.makedirs(logs_path, exist_ok=True)
+
+    handler = logging.FileHandler(os.path.join(logs_path, name + '.log'), 'a')
     formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
     handler.setFormatter(formatter)
     logger.addHandler(handler)
