ff69e2fde95157588bf9b87abcf2e83403ea02e1
Tomáš Mikula <mail@tomasmikula.cz>
2017-09-25 14:33:06 +0200

    Update setup.py requirements

--- setup.py
+++ setup.py
@@ -12,6 +12,17 @@ setup(name='UPOL-Crawler',
       license='MIT',
       url='https://github.com/UPOLSearch/UPOL-Crawler',
       packages=['upol_crawler'],
+      install_requires=[
+          'beautifulsoup4',
+          'celery',
+          'lxml',
+          'pymongo',
+          'pytest',
+          'reppy',
+          'requests',
+          'w3lib',
+          'langdetect'
+      ],
       entry_points={
           'console_scripts': [
               'upol_crawler = upol_crawler.__main__:main'
