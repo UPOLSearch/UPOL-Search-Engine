a8acc7d3ac59c1265d0591a6834e4e2c40f86687
Tomáš Mikula <mail@tomasmikula.cz>
2017-09-27 11:03:43 +0200

    Adjust start variables

--- upol_search_engine/__main__.py
+++ upol_search_engine/__main__.py
@@ -4,14 +4,35 @@ from upol_search_engine.upol_crawler import tasks
 
 
 def main():
-    crawler_settings = {'limit_domain': 'trnecka.inf.upol.cz',
-                'max_depth': 10,
-                'connect_max_timeout': 3.05,
-                'read_max_timeout': 10,
-                'frequency_per_server': 0.5,
-                'blacklist': ""}
+    blacklist = """portal.upol.cz
+    stag.upol.cz
+    library.upol.cz
+    adfs.upol.cz
+    portalbeta.upol.cz
+    idp.upol.cz
+    famaplus.upol.cz
+    es.upol.cz
+    smlouvy.upol.cz
+    menza.upol.cz
+    edis.upol.cz
+    courseware.upol.cz"""
 
-    seed = "http://trnecka.inf.upol.cz"
+    crawler_settings = {'limit_domain': 'inf.upol.cz',
+                        'max_depth': 10,
+                        'connect_max_timeout': 3.05,
+                        'read_max_timeout': 10,
+                        'frequency_per_server': 0.5,
+                        'blacklist': blacklist}
+
+    seed = """https://www.upol.cz
+    https://www.cmtf.upol.cz
+    https://www.lf.upol.cz
+    https://www.ff.upol.cz
+    https://www.prf.upol.cz
+    https://www.pdf.upol.cz
+    https://ftk.upol.cz
+    https://www.pf.upol.cz
+    https://www.fzv.upol.cz"""
 
     print("Launching crawler")
 
@@ -19,7 +40,7 @@ def main():
         crawler_settings=crawler_settings,
         seed=seed,
         batch_size=300,
-        delay_between_feeding=5)
+        delay_between_feeding=30)
 
     while feeder.status != 'SUCCESS':
         print(feeder.status)
