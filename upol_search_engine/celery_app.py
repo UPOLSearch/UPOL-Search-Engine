9b5ef5516f64ae925bf96bd31c0cd54822834d15
Tomáš Mikula <mail@tomasmikula.cz>
2017-10-11 11:12:29 +0200

    Test

--- upol_search_engine/celery_app.py
+++ upol_search_engine/celery_app.py
@@ -32,7 +32,7 @@ class Config(object):
     beat_schedule = {
         'run-search-engine': {
             'task': 'upol_search_engine.tasks.main_task',
-            'schedule': crontab(minute='*/2'),
+            'schedule': crontab(hour='*/1'),
             'args': ()
         }
     }
