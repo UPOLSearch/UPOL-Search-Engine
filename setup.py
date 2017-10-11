d2a7a52731f41458a78886fc2b5ab38285384ce4
Tomáš Mikula <mail@tomasmikula.cz>
2017-10-11 16:20:23 +0200

    Setup

--- setup.py
+++ setup.py
@@ -14,7 +14,10 @@ setup(name='UPOL-Search-Engine',
       package_data={
           'upol_search_engine': ['config-default.ini',
                                  'upol_search_engine/templates/*',
-                                 'upol_search_engine/static/*'],
+                                 'upol_search_engine/static/css/*',
+                                 'upol_search_engine/static/js/*',
+                                 'upol_search_engine/static/fonts/*',
+                                 'upol_search_engine/static/images/*'],
       },
       install_requires=[
           'beautifulsoup4',
