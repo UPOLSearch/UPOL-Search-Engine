b477ac66f0199e403fcc3e717961a50f9312f917
Tomáš Mikula <mail@tomasmikula.cz>
2017-10-11 21:26:37 +0200

    Time format

--- upol_search_engine/upol_search_engine/upol_search_engine.py
+++ upol_search_engine/upol_search_engine/upol_search_engine.py
@@ -23,7 +23,7 @@ def api_stats():
     def timedelta_to_string(timedelta):
         seconds = timedelta.total_seconds()
 
-        return '{}h {}m'.format(seconds // 3600, seconds % 3600 // 60)
+        return '{:.0f}h {:.0f}m'.format(seconds // 3600, seconds % 3600 // 60)
 
 
     mongodb_client = mongodb.create_client()
