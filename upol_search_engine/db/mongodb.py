879a046e04ee6bcb62c68accbb785e99e01c9865
Tomáš Mikula <mail@tomasmikula.cz>
2017-10-11 20:58:11 +0200

    Test

--- upol_search_engine/db/mongodb.py
+++ upol_search_engine/db/mongodb.py
@@ -597,7 +597,7 @@ def get_latest_stats(client):
         tz_aware=True,
         tzinfo=pytz.timezone('Europe/Prague')))
 
-    return aware_times.find().sort('$natural', pymongo.ASCENDING).limit(1)[0]
+    return aware_times.find().sort('$natural', pymongo.DESCENDING).limit(1)[0]
 
 
 def get_count_of_not_indexed(db):
