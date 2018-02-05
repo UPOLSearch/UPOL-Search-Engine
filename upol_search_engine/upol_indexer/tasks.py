94d900e5e844921244dd322f1b59eb86a170c6f8
Tomáš Mikula <mail@tomasmikula.cz>
2018-02-05 12:31:01 +0100

    Update

--- upol_search_engine/upol_indexer/tasks.py
+++ upol_search_engine/upol_indexer/tasks.py
@@ -6,7 +6,7 @@ def indexer_task(crawler_settings, indexer_settings, task_id):
     from upol_search_engine.db import postgresql
     import locale
     from celery.result import AsyncResult
-    from celery.states import PENDING, STARTED, SUCCESS
+    from celery.states import PENDING, STARTED, RECIEVED
     import time
 
     locale.setlocale(locale.LC_ALL, 'cs_CZ.utf-8')
@@ -65,7 +65,7 @@ def indexer_task(crawler_settings, indexer_settings, task_id):
         for task in tasks_list:
             state = AsyncResult(task.task_id).status
 
-            if state == PENDING or state == STARTED or state == SUCCESS:
+            if state == PENDING or state == STARTED or state == RECIEVED:
                 n_of_running += 1
             else:
                 tasks_list.remove(task)
